c3bd0d87ed9e05d84dfbd79356f4eea0f9f8e121
= <jy7123943@gmail.com>
2019-11-11 00:12:54 +0900

    feat: delete example files

--- examples/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './src/App';
-import { Bugcide } from '../src/index';
-
-Bugcide.init({ projectToken: 'my-react-new-project-LEzoc' });
-
-render(<App />, document.getElementById('root'));
